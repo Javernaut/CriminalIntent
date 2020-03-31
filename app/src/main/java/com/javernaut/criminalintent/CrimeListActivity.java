ebfe65766d5c41797b696abf4f3d43477bf217cc
Javernaut <javernaut@gmail.com>
2020-03-31 19:17:38 +0300

    Preparing data to display

--- app/src/main/java/com/javernaut/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/javernaut/criminalintent/CrimeListActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CrimeListActivity extends AppCompatActivity {
+
+    // Model
+    private List<Crime> crimesList = generateDemoCrimes();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static List<Crime> generateDemoCrimes() {
+        List<Crime> result = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Для каждого второго объекта mCrimes.add(crime);
+            result.add(crime);
+        }
+        return result;
     }
 }
